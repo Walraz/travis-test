b30d48101a2e6197b6fed1ba572bdf31b76a23e2
Rasmus <hallstrom.rasmus@gmail.com>
2016-06-09 12:44:20 +0200

    sucess

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var expect  = require("chai").expect;
 var calc = require('./main');
 
 describe('Calc', function() {
-  var number = calc.calc(11, 10);
+  var number = calc.calc(10, 10);
   it('should add two numbers', function() {
     expect(number).to.equal(20);
   });
