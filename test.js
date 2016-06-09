098ca97a904a33c9a00d6b75719762dbe43d964a
Rasmus <hallstrom.rasmus@gmail.com>
2016-06-09 12:57:37 +0200

    lol

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var expect  = require("chai").expect;
 var calc = require('./main');
 
 describe('Calc', function() {
-  var number = calc.calc(15, 15);
+  var number = calc.calc(10, 15);
   var number2 = calc.calc(0, 0);
   it('should add two numbers', function() {
     expect(number).to.equal(30);
