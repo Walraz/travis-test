375a4ab38c5a0d3a427a064e87bd3c2fe160ce2a
Rasmus <hallstrom.rasmus@gmail.com>
2016-06-09 12:53:29 +0200

    adding test

--- test.js
+++ test.js
@@ -2,9 +2,14 @@ var expect  = require("chai").expect;
 var calc = require('./main');
 
 describe('Calc', function() {
-  var number = calc.calc(10, 10);
+  var number = calc.calc(15, 15);
+  var number2 = calc.calc(0, 0);
   it('should add two numbers', function() {
-    expect(number).to.equal(20);
+    expect(number).to.equal(30);
+  });
+
+  it('should add two numbers', function() {
+    expect(number2).to.equal(0);
   });
 
 });
